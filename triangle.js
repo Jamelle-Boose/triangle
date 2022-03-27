9e7209482227ede8a0ae5f5a84436d2770530ab2
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-03-27 15:07:02 -0700

    refactor Fixes #1

--- triangle.js
+++ triangle.js
@@ -1,50 +1,28 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// An equilateral triangle has all three sides the same length.
-// An isosceles triangle has at least two sides the same length.
-// A scalene triangle has all sides of different lengths.
-
 export class Triangle {
   constructor(...sides) {
     this.sides = sides
+    this.isTriangle = this.isTriangle()
+  }
+
+  isTriangle() {
+    const [a, b, c] = this.sides
+    return a + b > c && a + c > b && b + c > a
   }
 
   get isEquilateral() {
-    const notZero = this.sides.reduce((a, b) => a + b)
-    const isEqual = this.sides.every(i => i === this.sides[0])
-
-    return isEqual && notZero ? true : false
-  }
-
-  get isIsosceles() {
-    let [side1, side2, side3] = this.sides
-    let isTriangle =
-      side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1
-        ? true
-        : false
-
-    return isTriangle && side2 === side3
-      ? true
-      : false || (isTriangle && side1 === side2)
-      ? true
-      : false || (isTriangle && side1 === side3)
-      ? true
-      : false || (isTriangle && (side1 === side2) === side3)
-      ? true
-      : false
+    const [a, b, c] = this.sides
+    return this.isTriangle && a == b && b == c
   }
 
   get isScalene() {
-    let [side1, side2, side3] = this.sides
-    let isTriangle =
-      side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1
-        ? true
-        : false
-    let isNotEqual = side1 !== side2 && side1 !== side3 && side2 !== side3
+    const [a, b, c] = this.sides
+    return this.isTriangle && a != b && b != c && a != c
+  }
 
-    return isTriangle && isNotEqual ? true : false
+  get isIsosceles() {
+    if (this.isScalene) return false
+
+    const [a, b, c] = this.sides
+    return 2 * Math.max(a, b, c) < a + b + c
   }
 }
